aeec47a4c2dbf7180579158c2ae80bc48b093241
supersixaxis <appert.nathan@outlook.fr>
2023-12-22 17:29:35 +0100

    edit user and esthetic

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import '../styles/User.css'
-export default function User({ user }) {
-    return (
-      <div className='cardUserContainer'>
-        <img src={user.photos} alt="" />
-        <h2>{user.firstName} {user.lastName}</h2>
-        <p>{user.age}</p>
-        <p>{user.gender}</p>
-      </div>
-    );
-  }
+import React, { useState, useEffect } from 'react';
+import '../styles/User.css';
+import EditUserForm from './EditUserForm';
+import '../styles/UserForm.css';
+export default function User({ user, onUpdateUser }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const startEditing = () => {
+    setIsEditing(true);
+  };
+  const cancelEditing = () => {
+    setIsEditing(false);
+  };
+  return (
+    <div className='cardUserContainer' onClick={startEditing}>
+      <img src={user.photos} alt="" />
+      <h2>{user.firstName} {user.lastName}</h2>
+      <p>{user.age}</p>
+      <p>{user.gender}</p>
+      {isEditing && (
+        <>
+         <div className="overlay active"></div>
+        <EditUserForm
+          user={user}
+          onUpdateUser={(editedUser) => {
+            onUpdateUser(user.id, editedUser);
+            cancelEditing();
+          }}
+          onCancelEdit={cancelEditing}
+        />
+        </>
+      )}
+    </div>
+  );
+}
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,50 +1,55 @@
-import React, { useState } from 'react'
-import User from './User'
-import AddUserForm from './AddUserForm'
+import React, { useState } from 'react';
+import User from './User';
+import AddUserForm from './AddUserForm';
 import { v4 as uuidv4 } from 'uuid';
+
 export default function Users() {
+  const [users, setUsers] = useState([
+    {
+      id: '1',
+      photos: 'https://picsum.photos/200',
+      lastName: 'Doe',
+      firstName: 'John',
+      age: '20',
+      gender: 'Male',
+    },
+    {
+      id: '2',
+      photos: 'https://picsum.photos/200',
+      lastName: 'Smith',
+      firstName: 'Jane',
+      age: '20',
+      gender: 'Male',
+    },
+  ]);
 
-    const [users, setUsers] = useState([
-        {
-            id: '1',
-            photos:'https://picsum.photos/200',
-            lastName: 'Doe',
-            firstName: 'John', 
-            age: '20',
-            gender: 'Male',
-        },
-        {
-            id: '2',
-            photos:'https://picsum.photos/200',
-            lastName: 'Smith',
-            firstName: 'Jane', 
-            age: '20',
-            gender: 'Male',
-        },
-    ])
+  const addUser = (lastName, firstName, age, gender) => {
+    const newUser = {
+      id: uuidv4(),
+      photos: 'https://picsum.photos/200',
+      lastName: lastName,
+      firstName: firstName,
+      age: age,
+      gender: gender,
+    };
+    setUsers((prevUserList) => [...prevUserList, newUser]);
+  };
 
-    const addUser = (lastName, firstName, age, gender) => {
-        const newUser = {
-          id: uuidv4(),
-          photos:'https://picsum.photos/200',
-          lastName: lastName,
-          firstName: firstName, 
-          age: age,
-          gender: gender
-        };
-        setUsers((prevUserList) => [...prevUserList, newUser]);
-      };
+  const updateUser = (userId, editedUser) => {
+    setUsers((prevUserList) =>
+      prevUserList.map((user) => (user.id === userId ? { ...user, ...editedUser } : user))
+    );
+  };
+
   return (
     <div className='container'>
-        <AddUserForm onAddUser={addUser} />
+      <AddUserForm onAddUser={addUser} />
       <h1>Liste des utilisateurs</h1>
       <div className='usersContainer'>
-      {users.map((user, index) => {
-          console.log('User data:', user);
-          return <User key={index} user={user} />;
-        })}
-
+        {users.map((user, index) => (
+          <User key={index} user={user} onUpdateUser={updateUser} />
+        ))}
       </div>
     </div>
-  )
+  );
 }
